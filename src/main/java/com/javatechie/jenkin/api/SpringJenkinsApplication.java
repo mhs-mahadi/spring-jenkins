6fa9f6ed85e43bee9a52b58f1b504a65b252207a
Shohag <mahadishohagbd@gmail.com>
2023-11-16 23:54:24 +0600

    new message added

--- src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
+++ src/main/java/com/javatechie/jenkin/api/SpringJenkinsApplication.java
@@ -12,7 +12,7 @@ public class SpringJenkinsApplication {
 
 	@GetMapping("/ping")
 	public String message() {
-		return "Wao!! Application Deployed successfully in jenkins pipeline.......";
+		return "Wao!! Application Deployed successfully in jenkins pipeline...helo....";
 	}
 
 	public static void main(String[] args) {
